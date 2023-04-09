376ae067f5b5d126e1cb50b55b8f0f2a36e811a2
lucaengelhard <me@lucaengelhard.com>
2023-04-09 16:52:06 +0200

    login file

---
+++ public/login.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logindata = void 0;
+exports.logindata = {
+    username: "",
+    password: "",
+};
---
+++ src/login.ts
@@ -0,0 +1,9 @@
+export interface Login {
+  username: string;
+  password: string;
+}
+
+export const logindata: Login = {
+  username: "",
+  password: "",
+};
