5381cfc39480df2ff8482655b1e45c29fef508b1
lucaengelhard <me@lucaengelhard.com>
2023-04-12 20:22:39 +0200

    delete compiled

--- public/login.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logindata = void 0;
-exports.logindata = {
-    username: "capmaennle",
-    password: "Computer2208.",
-};
