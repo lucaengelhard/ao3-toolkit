f89575ae1817021ff6064b62b74fffa0c09e1642
Luca Engelhard <me@lucaengelhard.com>
2023-04-14 11:18:05 +0200

    Update login.ts

--- src/config/login.ts
+++ src/config/login.ts
@@ -1,5 +1,9 @@
 import { Login } from '../types/base.js';
 
+/**
+ * Here the base user login to use in the rest of the module can be defined
+ *
+ */
 export let logindata: Login = {
   username: '',
   password: '',
