23fb540214c4ec26cf45dfccf8799e186f60dbed
Luca Engelhard <me@lucaengelhard.com>
2023-04-14 11:30:39 +0200

    Update export.ts

--- src/export.ts
+++ src/export.ts
@@ -1,43 +1,11 @@
-export { ao3 } from "./classes/base.js";
-export { Work, historyWork } from "./classes/works.js";
-export { logindata } from "../src/config/login.js";
-export { Login } from "./types/base.js";
+export { ao3 as default } from "./classes/base.js";
+export { * } from "./classes/works.js";
+export { * } from "../src/config/login.js";
+export { * } from "./types/base.js";
 export {
-  Author,
-  Fandom,
-  ChaptersWritten,
-  Relationship,
-  Character,
-  Rating,
-  archiveWarning,
-  Category,
-  Tag,
-  Series,
-  Collection,
-  Stats,
-  Notes,
-  Chapter,
-  Content,
-  Info,
-  HistoryElement,
+  *
 } from "./types/works";
 export { linkToAbsolute, getParsableInfoData } from "./utils/helper.js";
 export {
-  getWork,
-  getContent,
-  getInfo,
-  getTitle,
-  getAuthor,
-  getFandom,
-  getStats,
-  getRelationships,
-  getCharacters,
-  getRating,
-  getWarnings,
-  getCategories,
-  getTags,
-  getLanguage,
-  getSeries,
-  getCollections,
-  getSummary,
+*
 } from "./utils/works.js";
