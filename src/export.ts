fecc76177def67dec3dfb1e7ee587994a9783534
lucaengelhard <me@lucaengelhard.com>
2023-04-14 21:16:15 +0200

    export changes

--- src/export.ts
+++ src/export.ts
@@ -1,11 +1,7 @@
 export { ao3 as default } from "./classes/base.js";
-export { * } from "./classes/works.js";
-export { * } from "../src/config/login.js";
-export { * } from "./types/base.js";
-export {
-  *
-} from "./types/works";
-export { linkToAbsolute, getParsableInfoData } from "./utils/helper.js";
-export {
-*
-} from "./utils/works.js";
+export * from "./classes/works.js";
+export * from "../src/config/login.js";
+export * from "./types/base.js";
+export * from "./types/works";
+export * from "./utils/helper.js";
+export * from "./utils/works.js";
