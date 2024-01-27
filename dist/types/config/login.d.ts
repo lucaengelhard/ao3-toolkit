4bff0f981655e03f83e8699d2c43e3d327d03e3b
Luca Engelhard <me@lucaengelhard.com>
2024-01-27 11:50:17 +0100

    rewrite init

--- dist/types/config/login.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Login } from "../interfaces.js";
-export declare let logindata: Login;
-//# sourceMappingURL=login.d.ts.map
--- src/config/login.ts
+++
@@ -1,6 +0,0 @@
-import type { Login } from "../interfaces.js";
-
-export let logindata: Login = {
-  username: "",
-  password: "",
-};
