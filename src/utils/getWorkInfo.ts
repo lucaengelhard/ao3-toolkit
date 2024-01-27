422af0a407c8f4021764b06251dd312a461b9e18
lucaengelhard <me@lucaengelhard.com>
2024-01-27 18:08:34 +0100

    getRelationship function

--- src/utils/getWorkInfo.ts
+++ src/utils/getWorkInfo.ts
@@ -2,7 +2,12 @@ import * as cheerio from "cheerio";
 
 import WorkInfo from "../classes/ClassWorkInfo";
 import { getParsableInfodata, linkToAbsolute } from "./helpers";
-import { Author, Fandom, Title } from "../interfaces/InterfaceWorkInfo";
+import {
+  Author,
+  Fandom,
+  Relationship,
+  Title,
+} from "../interfaces/InterfaceWorkInfo";
 
 /**
  *
@@ -14,7 +19,12 @@ export default async function getWorkInfo(
 ): Promise<WorkInfo> {
   const work = await getParsableInfodata(input);
 
-  const parseFuntions = [getTitle(work), getAuthor(work)];
+  const parseFuntions = [
+    getTitle(work),
+    getAuthor(work),
+    getFandom(work),
+    getRelationship(work),
+  ];
 
   return Object.assign(await Promise.all(parseFuntions));
 }
@@ -66,6 +76,28 @@ export async function getFandom(
       return {
         fandomName: $(el).text(),
         fandomLink: linkToAbsolute($(el).attr("href")),
+      };
+    });
+}
+
+/**
+ *
+ * @param input - either a work id in form of a number or a parsable {@link cheerio.CheerioAPI} Object
+ * @returns - Array of {@link Relationship} objects cotaining information about the fandoms associated with the work
+ */
+export async function getRelationship(
+  input: number | cheerio.CheerioAPI
+): Promise<Relationship[]> {
+  let $: cheerio.CheerioAPI = await getParsableInfodata(input);
+
+  return $(".relationship")
+    .next()
+    .find("a")
+    .get()
+    .map((el) => {
+      return {
+        relationshipName: $(el).text(),
+        relationshipLink: linkToAbsolute($(el).attr("href")),
       };
     });
 }
