da4415b2b686d389543881b8289603c419a8d584
Luca Engelhard <me@lucaengelhard.com>
2024-01-31 10:01:27 +0100

    axios defaults

--- src/config/axiosDefaults.ts
+++ src/config/axiosDefaults.ts
@@ -1,9 +1,15 @@
+if (process.env.npm_package_name !== "ao3-toolkit") {
+  var header = `Axios/${process.env.npm_package_dependencies_axios} ${process.env.npm_package_name}/ao3-toolkit/Axios/${process.env.npm_package_dependencies_ao3_toolkit} bot`;
+} else {
+  var header = `Axios/${process.env.npm_package_dependencies_axios} ${process.env.npm_package_name}/${process.env.npm_package_version} bot`;
+}
+
 export const axiosDefaults = {
   batch: 10,
 
   axios: {
     headers: {
-      "User-Agent": "Axios/1.6.7 ao3-toolkit/2.0.0 bot",
+      "User-Agent": header,
     },
   },
 };
