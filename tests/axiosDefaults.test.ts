ed2f0be391a3c3a7b4b5fcd8aef09fd07c002ff2
Luca Engelhard <me@lucaengelhard.com>
2024-01-31 18:19:21 +0100

    fix imports

--- tests/axiosDefaults.test.ts
+++ tests/axiosDefaults.test.ts
@@ -1,4 +1,4 @@
-import { axiosDefaults } from "../dist";
+import { axiosDefaults } from "../src";
 
 test("get the name of the main module and add it to the axios request header", () => {
   console.log(axiosDefaults);
